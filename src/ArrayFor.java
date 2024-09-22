21c659a8720155f4556c2958b4e320d9d788aa35
VictorianoLeodriguez <ev_leodriguez@hotmail.com>
2024-09-22 08:14:06 -0300

    Update com For

---
+++ src/ArrayFor.java
@@ -0,0 +1,10 @@
+public class ArrayFor {
+    public static void main(String[] args) throws Exception {
+        String alunos[] = { "João", "Luana", "Julia", "Eduardo" };
+    
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        }
+    
+    }
+    }
