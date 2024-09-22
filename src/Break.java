e2a753731eed933c43e26bbbca88b96ec74f5a9b
VictorianoLeodriguez <ev_leodriguez@hotmail.com>
2024-09-22 08:24:43 -0300

    Break and continue update

---
+++ src/Break.java
@@ -0,0 +1,9 @@
+public class Break {
+    public static void main(String[] args) {
+        for (int numero = 0; numero <= 10; numero++){
+            if (numero == 5)
+                break;
+            System.out.println(numero);
+        }
+    }
+}
---
+++ src/Continue.java
@@ -0,0 +1,10 @@
+public class Continue {
+    public static void main(String[] args) {
+        for (int numero = 0; numero <= 10; numero++){
+            if (numero == 5)
+                continue;
+            System.out.println(numero);
+        }
+    }
+}
+
