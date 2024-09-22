3ee009153a53a8afa895bdc984313c7f515d8355
VictorianoLeodriguez <ev_leodriguez@hotmail.com>
2024-09-22 00:30:48 -0300

    Inicial_estruturas_de_repetição

---
+++ src/ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+
+    public static void main(String[] args) {
+        for (int carneiro = 1; carneiro <= 50; carneiro ++ ){
+            System.out.println(carneiro + " Caneirinhos ");
+
+        }
+        System.out.println("João dorme tranquilamente após contar seus carneiros");
+    }
+}
